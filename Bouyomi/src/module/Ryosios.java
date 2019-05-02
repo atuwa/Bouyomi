00c9f3c6d8ef00616fc3482dfb77394b42ac2745
atuwa <atuwasinji@gmail.com>
2019-05-03 00:22:07 +0900

    おっさん生きてる？をおっさん死んでる？でも反応するように

--- Bouyomi/src/module/Ryosios.java
+++ Bouyomi/src/module/Ryosios.java
@@ -1,26 +1,19 @@
 package module;
 
-import java.io.IOException;
-
 import bouyomi.DiscordAPI;
 import bouyomi.IModule;
 import bouyomi.Tag;
-import module.NicoAlart.Live;
 
 /**NicoAlartモジュールが無いとエラー吐くよ*/
 public class Ryosios implements IModule{
 
 	@Override
 	public void call(Tag tag){
-		if(tag.con.text.equals("おっさん生きてる？")) {
-			try{
-				Live[] lives=NicoAlart.getLives(null,1003067);
-				if(lives.length>0) {
-					DiscordAPI.chatDefaultHost("生きてる。良かった");
-					return;
-				}
-			}catch(IOException e){
-				e.printStackTrace();
+		if(tag.con.text.equals("おっさん生きてる？")||tag.con.text.equals("おっさん死んでる？")) {
+			String lives=NicoAlart.alarted.get("1003067");
+			if(lives!=null&&!lives.isEmpty()) {
+				DiscordAPI.chatDefaultHost("生きてる。良かった");
+				return;
 			}
 			String[] list= {"多分息してない/*もしかしたら生きてるかも\"","多分生きてない/*生命維持装置が故障してるかも"};
 			int i=new java.util.Random().nextInt(list.length);
