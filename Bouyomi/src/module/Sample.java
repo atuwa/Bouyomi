266936380bc840603aedbf7cf05f14a4a4fd7e5b
atuwa <atuwasinji@gmail.com>
2019-05-08 21:38:22 +0900

    再生禁止サンプルを修正

--- Bouyomi/src/module/Sample.java
+++ Bouyomi/src/module/Sample.java
@@ -3,6 +3,7 @@ package module;
 import bouyomi.DiscordAPI;
 import bouyomi.IModule;
 import bouyomi.Tag;
+import bouyomi.TubeAPI;
 import bouyomi.TubeAPI.PlayVideoEvent;
 import bouyomi.TubeAPI.PlayVideoTitleEvent;
 import bouyomi.Util;
@@ -23,7 +24,7 @@ public class Sample implements IModule{
 			DiscordAPI.chatDefaultHost(m+s.length());//メンションとタグの内容を連結して投稿
 		}
 		if(tag.con.mentions.contains("539105406107254804")) {
-			if(tag.con.text.contains("働け")||tag.con.text.contains("仕事しろ")) {
+			if(tag.con.text.equals("働け")||tag.con.text.equals("仕事しろ")) {
 				DiscordAPI.chatDefaultHost("やだ");
 			}
 		}
@@ -34,14 +35,33 @@ public class Sample implements IModule{
 			PlayVideoEvent e=(PlayVideoEvent)o;
 			//System.out.println("動画再生を検出"+e.videoID);
 			if(e.videoID.equals("nico=sm14223749")) {
-				DiscordAPI.chatDefaultHost("動画停止()/*この動画は再生禁止です");
+				//DiscordAPI.chatDefaultHost("動画停止()/*この動画は再生禁止です");
 				return;
 			}
 		}
 		if(o instanceof PlayVideoTitleEvent) {
 			PlayVideoTitleEvent e=(PlayVideoTitleEvent)o;
+			if("nico=sm14223749".equals(TubeAPI.lastPlay)){
+				new wait("動画停止()/*この動画は再生禁止です").start();
+				return;
+			}
 			//System.out.println("動画タイトルを取得："+e.title);
-			if(e.title.contains("オカリン"))DiscordAPI.chatDefaultHost("動画停止()/*タイトルに再生禁止ワードが含まれています");
+			if(e.title.contains("オカリン"))new wait("動画停止()/*タイトルに再生禁止ワードが含まれています").start();
+		}
+	}
+	private class wait extends Thread{
+		private String st;
+		public wait(String s) {
+			st=s;
+		}
+		@Override
+		public void run() {
+			try{
+				Thread.sleep(1500);
+			}catch(InterruptedException e){
+				e.printStackTrace();
+			}
+			DiscordAPI.chatDefaultHost(st);
 		}
 	}
 }
