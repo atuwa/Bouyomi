b50ba3d2e60079cef4828c0baa3a3c2966a1e9b6
atuwa <atuwasinji@gmail.com>
2019-05-22 08:14:50 +0900

    ドスケベウィンドウのデザインを変更

--- Bouyomi/src/module/Dosukebe.java
+++ Bouyomi/src/module/Dosukebe.java
@@ -1,6 +1,17 @@
 package module;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
 import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Polygon;
+import java.awt.Rectangle;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseWheelEvent;
 import java.awt.image.BufferedImage;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -13,8 +24,12 @@ import java.util.ArrayList;
 import java.util.Random;
 
 import javax.imageio.ImageIO;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
+import javax.swing.JPanel;
+import javax.swing.JRootPane;
+import javax.swing.border.LineBorder;
 
 import bouyomi.BouyomiProxy;
 import bouyomi.DailyUpdate;
@@ -33,6 +48,7 @@ public class Dosukebe implements IModule,IDailyUpdate,IAutoSave{
 	private Random rundom=new SecureRandom();
 	private static final int min=1500,max=5000;//最低15%最大50%
 	private int now=rundom.nextInt(max-min)+min;
+	private static BufferedImage img;
 	private boolean saved=false;
 	private int up;
 	public Dosukebe(){
@@ -49,6 +65,12 @@ public class Dosukebe implements IModule,IDailyUpdate,IAutoSave{
 		}catch(NumberFormatException nfe) {
 
 		}
+		try{
+			img=ImageIO.read(new File("mona.png"));
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+		PlayThread.frame.bound();
 	}
 	@Override
 	public void autoSave() throws IOException{
@@ -228,21 +250,17 @@ public class Dosukebe implements IModule,IDailyUpdate,IAutoSave{
 		}
 	}
 	private static class PlayThread extends Thread{
-		private static JFrame frame=new JFrame("Dosukebe");
+		private static DosukebeWindow frame;
 		private static ArrayList<URL> tasks=new ArrayList<URL>();
 		private static PlayThread thread;
-		private static JLabel label;
 		public PlayThread() {
 			super("PlayThread");
 			//frame.setTitle("Dosukebe");
 		}
 		private static void init(){
-			frame.getContentPane().setLayout(new FlowLayout());
-			label = new JLabel("未再生");
-			frame.getContentPane().add(label);
-			frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-			frame.setSize(50,50);
-			frame.setVisible(true);
+			//OSのウィンドウ装飾を無くして、Look&Feelの装飾にしておきます。
+			JFrame.setDefaultLookAndFeelDecorated(true);
+			frame=new DosukebeWindow("Dosukebe");
 			File f=new File("Dosukebe.png");
 			if(f.exists())try{
 				BufferedImage image=ImageIO.read(f);
@@ -274,11 +292,13 @@ public class Dosukebe implements IModule,IDailyUpdate,IAutoSave{
 					startmes.append("曲");
 				}
 				DiscordAPI.chatDefaultHost(startmes.toString());
-				label.setText("再生中："+name);
+				frame.setText("再生中："+name);
+				frame.bound();
 				System.out.println("再生開始："+name);
 				WAVPlayer.play(url.toString());
 				System.out.println("再生終了");
-				label.setText("終了："+name);
+				frame.setText("終了："+name);
+				frame.bound();
 				boolean end=true;
 				for(int i=0;i<100;i++) {
 					if(tasks.size()>0) {
@@ -294,6 +314,116 @@ public class Dosukebe implements IModule,IDailyUpdate,IAutoSave{
 				if(end)break;
 			}
 			thread=null;
+		}
+	}
+	private static class DosukebeWindow extends JFrame{
+		private double x,y=0;
+		private double scale=1;
+		private String text="未再生";
+		private int w,h;
+		public DosukebeWindow(String string) {
+			super(string);
+			Canvas sc=new Canvas();
+			setContentPane(sc);
+			JRootPane root=this.getRootPane();
+			root.setBorder(new LineBorder(Color.black, 0));
+			root.setWindowDecorationStyle(JRootPane.PLAIN_DIALOG);
+			JLayeredPane layeredPane = root.getLayeredPane();
+		    Component c = layeredPane.getComponent(1);
+		    if (c instanceof JComponent) {
+		      JComponent orgTitlePane = (JComponent) c;
+		      orgTitlePane.removeAll();
+		      orgTitlePane.setLayout(new BorderLayout());
+		      orgTitlePane.add(new JPanel(new BorderLayout()));
+		    }
+			getContentPane().setLayout(new FlowLayout());
+			setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+			setSize(500,500);
+			setVisible(true);
+			mouseInput mouseInput=new mouseInput();
+			this.addMouseListener(mouseInput);
+			this.addMouseWheelListener(mouseInput);
+			this.addMouseMotionListener(mouseInput);
+	        //ウィンドウ範囲を示す枠をつけておく
+	        //this.getRootPane().setBorder(new LineBorder(Color.black, 2));
+			this.repaint();
+		}
+		public void setText(String s){
+			text=s;
+		}
+		protected void bound() {
+			BufferedImage bi=new BufferedImage(2,2,BufferedImage.TYPE_4BYTE_ABGR);
+			Graphics2D g=bi.createGraphics();
+			g.setFont(new Font(null,Font.BOLD, (int) (40*scale)));
+			w=g.getFontMetrics(g.getFont()).stringWidth(text);
+			h=(int) (100*scale);
+			Rectangle b=getBounds();
+			if(img!=null)setBounds(b.x,b.y,(int)(img.getWidth()*scale)+w+8,(int)(img.getHeight()*scale)+h);
+			//b=getBounds();
+			//System.out.println("大きさ"+b.width+"x"+b.height+"="+(b.width*b.height)+"px");
+			//背景色を透明にします。
+			//ウィンドウ装飾を無くしておかないとjre1.7からはエラーが発生します。
+			setBackground(new Color(0,0,0,0));
+			this.repaint();
+		}
+		public class Canvas extends JPanel {
+			@Override
+			public void paintComponent(Graphics g) {
+				super.paintComponent(g);
+				g.setFont(new Font(null,Font.BOLD, (int) (40*scale)));
+				int imgw=0;
+				if(img!=null)g.drawImage(img,0,0,imgw=(int)(img.getWidth()*scale),(int)(img.getHeight()*scale),null);
+				g.setColor(Color.white);
+				//g.fillRect(imgw,0,w-2,h);
+				Polygon p=new Polygon();
+				int framew=g.getFontMetrics(g.getFont()).stringWidth(text)+1;
+				p.addPoint(imgw,0);
+				p.addPoint(imgw,h);
+				p.addPoint(imgw+framew/2-framew/6,h);
+				p.addPoint(imgw+framew/4,(int) (h*1.5));
+				p.addPoint(imgw+framew-2-framew/4,h);
+				p.addPoint(imgw+framew-2,h);
+				p.addPoint(imgw+framew-2,0);
+				g.fillPolygon(p);
+				g.setColor(Color.black);
+				//g.drawRect(imgw,0,w-2,h);
+				g.drawPolygon(p);
+				int h=g.getFontMetrics(g.getFont()).getHeight();
+				g.drawString(text,imgw,h);
+				//g.fillRect(0,0,1000,1000);
+			}
+		}
+		private class mouseInput extends MouseAdapter{
+			int px,py;
+			public void mouseWheelMoved(MouseWheelEvent e){
+				scale+=e.getWheelRotation()/20d;
+				if(scale<0.1)scale=0.1;
+				else if(scale>5)scale=5;
+				//System.out.println("拡大率"+scale+"倍");
+				bound();
+			}
+			public void mouseReleased(MouseEvent e){
+
+			}
+			public void mousePressed(MouseEvent e){
+				px=e.getX();
+				py=e.getY();
+				if(e.getButton()!=MouseEvent.BUTTON1) {
+					setAlwaysOnTop(!isAlwaysOnTop());
+				}
+			}
+			@Override
+			public void mouseMoved(MouseEvent e){
+				//System.out.println("mouseMoved");
+			}
+			@Override
+			public void mouseDragged(MouseEvent e){
+				//System.out.println("mouseDragged");
+				Rectangle b=getBounds();
+				y=b.y-py+e.getY();
+				x=b.x-px+e.getX();
+				setBounds((int)x,(int)y,b.width,b.height);
+			}
 		}
 	}
 }
